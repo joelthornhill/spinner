c7243049d54c45dadf564e62247e9d4b055ebd41
Joel Thornhill <joel.thornhill@ovoenergy.com>
2020-03-24 17:41:41 +0000

    Read from file

--- src/main/scala/example/SpinParser.scala
+++ src/main/scala/example/SpinParser.scala
@@ -6,6 +6,7 @@ import example.ParserCombinator._
 import cats.implicits._
 import org.andrewkilpatrick.elmGen.simulator.SpinSimulator
 
+import scala.io.Source
 import scala.util.Try
 import scala.util.parsing.combinator._
 
@@ -164,72 +165,15 @@ object App extends IOApp with EquParser with SpinParser {
 
   def run(args: List[String]): IO[ExitCode] = {
 
-    val s: String =
-      """
-        ;sample reverb program for FV-1
-        ;minimize number of delays and ops.
-        ;4 aps driving 2 AP-delay loops
-        ;drive both loop elements, take output from each
-        ;no pot controls
-        ;output is full reverb, not mixed
-        ;22 operations (of 128)
-
-        mem	api1	122
-        mem	api2	303
-        mem	api3	553
-        mem	api4	922
-
-        mem	ap1	3823
-        mem	del1	8500	;input = left output
-
-        mem	ap2	4732
-        mem	del2	7234	;input = right output
-
-        equ	krt	0.7	;adjust reverb time
-        equ	kap	0.625	;adjust AP coefficients
-        equ	apout	reg0	;holding reg input AP signal
-
-        ;input all passes (2)
-
-        rdax	adcl,0.25	;read inputs,
-        rdax	adcr,0.25	;attenuate, sum and
-        rda	api1#,kap	;do 4 APs
-        wrap	api1,-kap
-        rda	api2#,kap
-        wrap	api2,-kap
-        rda	api3#,kap
-        wrap	api3,-kap
-        rda	api4#,kap
-        wrap	api4,-kap
-        wrax	apout,1		;write to min, keep in ACC
-
-        ;first loop apd:
-        			;AP'd input in ACC
-        rda	del2#,krt	;read del2, scale by Krt
-        rda	ap1#,-kap	;do loop ap
-        wrap	ap1,kap
-        wra	del1,1.99	;write delay, x2 for dac out
-        wrax	dacl,0
-
-        ;second loop apd:
-
-        rdax	apout,1		;get input signal again
-        rda	del1#,krt	;as above, to other side of loop
-        rda	ap2#,kap
-        wrap	ap2,-kap
-        wra	del2,1.99
-        wrax	dacr,0
-      """.stripMargin
-
-    val getLines: List[String] = s
+    def getLines(s: String): List[String] = s
       .split("\n")
       .toList
       .map(removeComment)
       .filterNot(_.startsWith(";"))
       .filterNot(_.isEmpty)
 
-    val constants: IO[Map[String, EquValue]] = IO(
-      getLines
+    def constants(s: String): IO[Map[String, EquValue]] = IO(
+      getLines(s)
         .flatMap(
           line =>
             parse(equParser, line) match {
@@ -244,8 +188,8 @@ object App extends IOApp with EquParser with SpinParser {
 
     val instructions = new Instructions()
 
-    val spinParse: IO[List[Instruction]] = IO(
-      getLines
+    def spinParse(s: String): IO[List[Instruction]] = IO(
+      getLines(s)
         .flatMap(
           line =>
             parse(parsed(instructions), line) match {
@@ -260,11 +204,17 @@ object App extends IOApp with EquParser with SpinParser {
     )
 
     val testWav = "/tmp/test.wav"
+    val filePath = "/tmp/test.spn"
+
+    // TODO: make this and the simulator a resource
+    val fileContents = IO(Source.fromFile(filePath).getLines.mkString("\n"))
 
     val program = for {
-      consts <- constants
+      file <- fileContents
+      _ <- IO(println(file))
+      consts <- constants(file)
       _ <- printVals(consts)
-      parsed <- spinParse
+      parsed <- spinParse(file)
       _ <- printInstructions(parsed)
       _ <- runInstructions(parsed, consts)
       _ <- printRunInstructions(parsed, consts)
